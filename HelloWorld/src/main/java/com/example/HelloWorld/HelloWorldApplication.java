b61534c403bc8aa3fafa60222d476db4d95253d8
Ganesh Bute <ganesh.bute@dekatc.com>
2021-02-16 11:06:04 +0530

    added controller in seperate file.

--- HelloWorld/src/main/java/com/example/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -3,19 +3,15 @@ package com.example.HelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-
-@RestController
-public class HelloWorld {
-
-    @GetMapping(path="/")
-    public String hello(){
-        return "Goto Mars unless It is occupied by aliens. Then go to Jupiter.";
-    }
-
 }
+
+
+
+
